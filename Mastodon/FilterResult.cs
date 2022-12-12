9475c47818e385aa50ff1280bb5b5d464de60a03
Zan <thesonicrain@gmail.com>
2022-12-12 14:47:25 +0330

    Implements FilterResult class.

--- Mastodon/FilterResult.cs
+++ Mastodon/FilterResult.cs
@@ -1,6 +1,22 @@
 ﻿namespace Mastodon;
 
+/// <summary>
+/// Represents a filter whose keywords matched a given status.
+/// </summary>
 public sealed class FilterResult
 {
+    /// <summary>
+    /// The filter that was matched.
+    /// </summary>
+    public required Filter Filter { get; set; }
 
+    /// <summary>
+    /// The keyword within the filter that was matched.
+    /// </summary>
+    public List<string>? KeywordMatches { get; set; }
+
+    /// <summary>
+    /// The status ID within the filter that was matched.
+    /// </summary>
+    public List<string>? StatusMatches { get; set; }
 }
