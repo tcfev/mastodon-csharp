bb785d0af668194b5e438235ffa4fb83ecd553c9
zanzendegi <116729173+zanzendegi@users.noreply.github.com>
2022-12-12 16:54:20 +0330

    Basic interfaces for Instance, Media, Status and Account. (#33)

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,6 +1,8 @@
 ﻿var client = new Mastodon.MastodonClient(new Uri("https://mastodon.lol"));
 
 var statuses = await client.Timeline.GetPublicAsync();
-var instance = await client.GetInstanceAsync();
+var instance = await client.Instance.GetInstanceAsync();
+var extendedDescription = await client.Instance.GetExtendedDescriptionAsync();
+//var media = await client.Media.GetMediaAsync("109500199520793100");
 
 Console.WriteLine(statuses);
