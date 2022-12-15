9874510450b943d07256c3df8b080071c65a7f56
Zan <thesonicrain@gmail.com>
2022-12-15 14:36:52 +0330

    Implements gRPC => REST proxy for Account info.

---
+++ Mastodon/RoleExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using Mastodon.Grpc;
+
+namespace Mastodon;
+
+public static class RoleExtensionMethods
+{
+    public static Grpc.Role ToGrpc(this Mastodon.Models.Role i)
+    {
+        return new Role
+        {
+            Id = i.Id,
+        };
+    }
+}
